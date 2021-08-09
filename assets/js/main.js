fc47330af1db07aef1be04d9b76ddb463fb107b2
Imran <mahmud.bubt.150@gmail.com>
2021-08-09 12:20:02 +0600

    Added Required Files

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let str = "https://www.dropbox.com/s/313umwml79yfd0s/petzone.WordPress.2021-02-18.xml?dl=0";  
+// var str1 = document.getElementById("paste").innerHTML ;
+var str2 = str.substring(23, str.length - 5);
+
+// str1=str2;
+// document.getElementById("paste").innerHTML = str2;
