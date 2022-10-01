91063f1d7f71b8935a9c019212c1a1a49cfc6590
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-02 04:21:09 +0600

    Added GitHub Image Generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
-let str = "https://www.dropbox.com/s/313umwml79yfd0s/petzone.WordPress.2021-02-18.xml?dl=0";  
-// var str1 = document.getElementById("paste").innerHTML ;
-var str2 = str.substring(23, str.length - 5);
+var ret = "download-link-generator/blob/main/assets/img/Google-Drive.png".replace('/blob','');
+const username = 'devmhimran/'
+const githubLink = "https://raw.githubusercontent.com/"
+console.log(githubLink+ username+ ret);   
 
-// str1=str2;
-// document.getElementById("paste").innerHTML = str2;
